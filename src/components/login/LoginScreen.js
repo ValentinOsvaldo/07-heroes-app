df548c89f1f5ad668ebc1329bc936594e3b516ab
Osvaldo Valentin Garcia <co91.valentingarciaosvaldo@gmail.com>
2022-01-11 17:28:05 -0600

    rutas

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { types } from '../../types/types';
+import { AuthContext } from '../../auth/authContext';
 
 const LoginScreen = () => {
   const navigate = useNavigate();
+  const { dispatch } = useContext(AuthContext);
 
   const handleLogin = () => {
+    const action = {
+      type: types.login,
+      payload: {
+        name: 'Osvaldo',
+      },
+    };
+    
+    dispatch( action );
+    
     navigate('/', {
       replace: true,
     });
   };
 
   return (
-    <div className='container mt-5'>
+    <div className="container mt-5">
       <h1>Login</h1>
       <hr />
 
-      <button className='btn btn-primary' onClick={handleLogin}>
+      <button className="btn btn-primary" onClick={handleLogin}>
         login
       </button>
     </div>
